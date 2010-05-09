9f330230b64ccace3dc02aafe346ae0c0eece91f
Michal Trybus <komar007@gmail.com>
2010-05-09 18:22:08 +0200

    [gfx] Changed default format of surface to RGB 32b

--- gfx.c
+++ gfx.c
@@ -8,7 +8,8 @@ SDL_Surface *read_gfx(const char *path)
 	uint8_t *buffer;
 	size_t size;
 	SDL_RWops *rw;
-	SDL_Surface *gfx = NULL;
+	SDL_Surface *bmp = NULL,
+		    *gfx = NULL;
 
 	fp = fopen(path, "r");
 	if (!fp) {
@@ -36,14 +37,17 @@ SDL_Surface *read_gfx(const char *path)
 	memcpy(buffer, "BM", 2);
 	rw = SDL_RWFromMem(buffer, size);
 	assert(rw != NULL);
-	gfx = SDL_LoadBMP_RW(rw, 0);
-	if (!gfx) {
+	bmp = SDL_LoadBMP_RW(rw, 0);
+	if (!bmp) {
 		SDL_FreeRW(rw);
 		goto cleanup;
-	}
-	uint32_t color = SDL_MapRGB(gfx->format, 179, 179, 0);
-	SDL_SetColorKey(gfx, SDL_SRCCOLORKEY | SDL_RLEACCEL, color);
+	};
+	SDL_SetColorKey(bmp, SDL_SRCCOLORKEY, 0);
 	SDL_FreeRW(rw);
+	gfx = SDL_CreateRGBSurface(0, bmp->w, bmp->h, 32,
+			0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000);
+	SDL_BlitSurface(bmp, NULL, gfx, NULL);
+	SDL_FreeSurface(bmp);
 cleanup:
 	fclose(fp);
 	free(buffer);
