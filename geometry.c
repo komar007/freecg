af5475fcf75074d89fb1ff456fca8837b19af96a
Michal Trybus <komar007@gmail.com>
2010-06-25 21:08:49 +0200

    [geometry] minimal refactoring

--- geometry.c
+++ geometry.c
@@ -37,13 +37,12 @@ void ship_to_tile(const struct ship *s, struct tile *t)
 {
 	t->w = SHIP_W, t->h = SHIP_H;
 	t->x = (int)round(s->x), t->y = (int)round(s->y);
-	t->tex_x = 0, t->tex_y = 92;
 	if (s->engine) {
-		t->tex_x += SHIP_ON_IMG_X;
-		t->tex_y += SHIP_ON_IMG_Y;
+		t->tex_x = SHIP_ON_IMG_X;
+		t->tex_y = SHIP_ON_IMG_Y;
 	} else {
-		t->tex_x += SHIP_OFF_IMG_X;
-		t->tex_y += SHIP_OFF_IMG_Y;
+		t->tex_x = SHIP_OFF_IMG_X;
+		t->tex_y = SHIP_OFF_IMG_Y;
 	}
 	double drot = s->rot + M_PI/2;
 	normalize_angle(&drot);
